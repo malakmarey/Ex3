f6b489512f7142439f8caa340e623c56d1ee48e5
malakmarey <45039969+malakmarey@users.noreply.github.com>
2019-04-16 17:11:30 +0300

    Update Unit.h

--- Unit.h
+++ Unit.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 using namespace std;
 
@@ -8,12 +7,11 @@ namespace ariel{
 enum Unit {
     
 
-    CM , M , KM , SEC , MIN, HOUR, G ,KG ,TON , count
+    CM=1 , M , KM , SEC , MIN, HOUR, G ,KG ,TON ,count
 };
 
 const long unit_values[9] = {1, 100, 100000, 1, 60, 3600, 1, 1000, 1000000};
 const string unit_types[9] = {"cm", "m", "km", "sec", "min", "hour", "g", "kg", "ton"};
-
 
 
 };
