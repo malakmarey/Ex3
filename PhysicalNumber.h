a66e9a51410e01a19afb79253294ed88c2ab0d6e
malakmarey <45039969+malakmarey@users.noreply.github.com>
2019-04-16 17:07:04 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -1,54 +1,55 @@
 #pragma once
-
 #include <iostream>
 #include "Unit.h"
 
 namespace ariel{
-
 class PhysicalNumber {
+
+private:
 
 long double value_number;
 Unit unit_type;
 
-//---------------------------------
-//Helping Functions
-//---------------------------------
-
-friend bool checkType (const PhysicalNumber& phy1 ,const PhysicalNumber& phy2);
-
 
 public:
+
 //---------------------------------
 //constractors
 //---------------------------------
+
 Unit getUserType() {
     return unit_type;
 }
 
 
-PhysicalNumber(long double,Unit);
-~PhysicalNumber();
-PhysicalNumber() = default;
+PhysicalNumber(long double value_number,Unit unit_type){
+    {
+            this->value_number = value_number;
+            this->unit_type = unit_type;
+        }
+}
+~PhysicalNumber(){}
+
 
 //---------------------------------
 // +,- +=  Math opreation , ect..
 //---------------------------------
 
-const PhysicalNumber operator+(const PhysicalNumber& phyNum) const;
-const PhysicalNumber operator-(const PhysicalNumber& phyNum) const;
+ PhysicalNumber operator+(const PhysicalNumber& phyNum) ;
+ PhysicalNumber operator-(const PhysicalNumber& phyNum) ;
 
 
 PhysicalNumber& operator+=(const PhysicalNumber& phyNum);
 PhysicalNumber& operator-=(const PhysicalNumber& phyNum);
-PhysicalNumber& operator=(const PhysicalNumber& phyNum);
+// PhysicalNumber& operator=(const PhysicalNumber& phyNum);
 
 //---------------------------------
 // onary preations
 //---------------------------------
 
 
-const PhysicalNumber operator+() const ;
-const PhysicalNumber operator-() const ;
+const PhysicalNumber operator+()  ;
+const PhysicalNumber operator-()  ;
 
 //---------------------------------
 // < , > <= , >=  friends globals opreations
@@ -88,9 +89,16 @@ bool operator>(const PhysicalNumber& phy,const PhysicalNumber& phy1);
 bool operator<=(const PhysicalNumber& phy,const PhysicalNumber& phy1);
 bool operator>=(const PhysicalNumber& phy,const PhysicalNumber& phy1);
 bool operator!=(const PhysicalNumber& phy,const PhysicalNumber& phy1);
-bool checkType (const PhysicalNumber& phy1 ,const PhysicalNumber& phy2);
 
 ostream& operator<<(ostream& os, const PhysicalNumber& c);
 istream& operator>>(istream& is, PhysicalNumber& c);
+Unit get_Unit(int unit_number);
+bool checkType (Unit unit_a, Unit unit_b);
+double getdata_number(string str);
+double double_base(double data_number, Unit temp_unit);
+double unit_double(double data_number, Unit unit_temp);
+int size_uni(string str);
+int verif_string_unit(string str);
+int data_numberCorrect(string str);
 
 }
